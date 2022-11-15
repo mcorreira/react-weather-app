9507b946a5425a62a9da529c2de456dc2b6d7aa4
mcorreira <meganmcorreira@gmail.com>
2022-11-15 16:20:00 -0500

    Added link to git

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
 
 function App() {
-  return <div className="Weather App">Weather App</div>;
+  return (
+    <div className="Weather App">
+      Weather App
+      <footer>
+        This project was coded by Megan Correira and is{" "}
+        <a
+          href="https://github.com/mcorreira/react-weather-app"
+          target="_blank"
+        >
+          {" "}
+          open sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
